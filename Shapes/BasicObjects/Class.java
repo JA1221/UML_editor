5391296281d6ea9545e04ce9e9f930be169187ec
JA1221 <cat605101@yahoo.com.tw>
2021-04-06 00:16:02 +0800

    Update Class.Draw()

--- Shapes/BasicObjects/Class.java
+++ Shapes/BasicObjects/Class.java
@@ -1,11 +1,12 @@
 package Shapes.BasicObjects;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
 
 public class Class extends BasicObjects{
     private final int DefaultWidth = 80;
-    private final int DefaultHeight = 130;
+    private final int DefaultHeight = 120;
 
     public Class(int X, int Y) {
         super(X, Y);
@@ -19,9 +20,13 @@ public class Class extends BasicObjects{
 
     @Override
     public void Draw(Graphics g) {
+        g.setColor(Color.WHITE);
+        g.fillRect(X, Y, DefaultWidth, DefaultHeight);
+
+        g.setColor(Color.BLACK);
         g.drawRect(X, Y, DefaultWidth, DefaultHeight / 3);
         g.drawRect(X, Y + DefaultHeight / 3, DefaultWidth, DefaultHeight / 3);
-        g.drawRect(X, Y + 2 * DefaultHeight / 3, DefaultWidth, DefaultHeight / 3);
+        g.drawRect(X, Y + 2 * DefaultHeight / 3, DefaultWidth, DefaultHeight / 3);      
 
         DrawPoints(g);
     }
